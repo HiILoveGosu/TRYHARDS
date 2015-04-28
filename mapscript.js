6377b2ae11f94279d186830c89e3f0a8b01a554c
HiIloveGosu <jari.schuurman@live.nl>
2015-04-28 13:11:18 +0200

    marker fix
    asd

--- mapscript.js
+++ mapscript.js
@@ -3,14 +3,31 @@ function initialize() {
     var mapOptions = {
         zoom: 13,
         center: myLatlng
-    }
+    };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    $.getJSON("http://localhost:8080/TRYHARDS/data.json")
+        .done(function (data) {
+            $.each( data.delicten, function(i, value) {
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'Hello World!'
-    });
+                var myLatlng = new google.maps.LatLng(value.latitude, value.longitude);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: "text "+value.titel
+                });
+                var infowindow = new google.maps.InfoWindow({
+                    content: value.omschrijving
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.open(map,marker);
+                });
+
+            });
+            console.log(data);
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            console.log('Data ophalen mislukt');
+        });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
